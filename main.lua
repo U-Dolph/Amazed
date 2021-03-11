bfc6a9f7a94b3543735df0a2024aec1e7cdae103
Madarász Tamás <m.tamas1012@gmail.com>
2021-03-11 13:43:22 +0100

    reworked A*

--- main.lua
+++ main.lua
@@ -3,11 +3,68 @@ require "mazeGenerator"
 mazeWidth = 20
 mazeHeight = 20
 drawScale = math.floor(math.min((love.graphics.getWidth() - 20) / (mazeWidth + 1), (love.graphics.getHeight() - 20) / (mazeHeight + 1)))
+Scale = 1
+OffsetX = 0
+OffsetY = 0
 
 function love.load()
 	stuff = createMaze(mazeWidth, mazeHeight)
+	love.graphics.setDefaultFilter("nearest", "nearest")
 
 	timer = 0
+
+	img = love.graphics.newImage("gfx/0x72_DungeonTilesetII_v1.3.png")
+
+	floor = love.graphics.newQuad(16, 64, 16, 16, img:getDimensions())
+
+	wallLeft = love.graphics.newQuad(16, 128, 16, 16, img:getDimensions())
+	wallLeftCorner = love.graphics.newQuad(32, 128, 16, 16, img:getDimensions())
+
+	wallRight = love.graphics.newQuad(0, 128, 16, 16, img:getDimensions())
+	wallRightCorner = love.graphics.newQuad(48, 128, 16, 16, img:getDimensions())
+
+	wallTop = love.graphics.newQuad(16, 16, 16, 16, img:getDimensions())
+
+	tilemap = {}
+
+	for y = 0, mazeHeight - 1 do
+		for x = 0, mazeWidth - 1 do
+			local room = {tiles = {}, x = x * 16 * 16, y = y * 16 * 16}
+			table.insert(tilemap, room)
+		end
+	end
+
+	for i, j in ipairs(tilemap) do
+		if not stuff.rooms[i].previsited then
+			for k = 0, 15 do
+				for l = 0, 15 do
+					table.insert(j.tiles, {floorImage = floor, wallImage = nil, x = j.x + l * 16, y = j.y + k * 16})
+				end
+			end
+		end
+
+		for k, l in ipairs(j.tiles) do
+			if stuff.rooms[i].path[1] == 0 and l.y == j.y then
+				l.wallImage = wallTop
+			end
+
+			if stuff.rooms[i].path[4] == 0 and l.x == j.x then
+				l.wallImage = wallLeft
+
+				if stuff.rooms[i].path[1] == 0 and j.x == l.x and j.y == l.y then
+					l.wallImage = wallLeftCorner
+				end
+			end
+
+			if stuff.rooms[i].path[2] == 0 and l.x == j.x + 16 * 15 then
+				l.wallImage = wallRight
+
+				if stuff.rooms[i].path[1] == 0 and l.x == j.x + 16 * 15 and l.y == j.y then
+					l.wallImage = wallRightCorner
+				end
+			end
+		end
+	end
 end
 
 function love.update(dt)
@@ -19,11 +76,35 @@ function love.draw()
 	love.graphics.translate(1, 1)
 
 	stuff.render(stuff, drawScale)
-    love.graphics.reset()
+	love.graphics.reset()
+
+	love.graphics.scale(Scale)
+	love.graphics.translate(OffsetX, OffsetY)
+	--[[for i, j in ipairs(tilemap) do
+		for k, l in ipairs(j.tiles) do
+			love.graphics.draw(img, l.floorImage, l.x, l.y)
+			if l.wallImage then
+				love.graphics.draw(img, l.wallImage, l.x, l.y)
+			end
+		end
+	end]]
+
+	love.graphics.reset()
 end
 
 function love.resize()
 	drawScale = math.floor(math.min((love.graphics.getWidth()) / (mazeWidth + 1), (love.graphics.getHeight()) / (mazeHeight + 1)))
+end
+
+function love.wheelmoved(x, y)
+	Scale = math.max(Scale + y / 20, 0.05)
+end
+
+function love.mousemoved(x, y, dx, dy)
+	if love.mouse.isDown(1) then
+		OffsetX = OffsetX + dx / Scale
+		OffsetY = OffsetY + dy / Scale
+	end
 end
 
 function love.keypressed(key)
--- mazeGenerator.lua
+++ mazeGenerator.lua
@@ -3,13 +3,8 @@ local noiseOffsetX, noiseOffsetY = love.math.random(-5000, 5000), love.math.rand
 
 local stack = {}
 
-local startNode = nil
-local endNode = nil
-
-local function getIndex(_modX, _modY, _element)
-    if not _element then _element = stack[#stack] end
-    return (_element.y + _modY) * width + (_element.x + _modX) + 1
-end
+local startNode, endNode = nil, nil
+local otherStartNode, otherEndNode = nil, nil
 
 local maze = {
     rooms = {},
@@ -21,31 +16,26 @@ local maze = {
         love.graphics.rectangle("line", 0 - _lW / 2, 0 - _lW / 2, width, height)
 
         for i, j in ipairs(self.rooms) do
-            local mx, my = love.mouse.getPosition()
-
-            if mx >= j.x * _scale + _scale and mx < j.x * _scale + _scale + _scale and my >= j.y * _scale + _scale and my < j.y * _scale + _scale + _scale then
-                if endNode ~= self.nodes[i] and love.mouse.isDown(2) then
-                    endNode = self.nodes[i]
-                end
-
-                if startNode ~= self.nodes[i] and love.mouse.isDown(1) then
-                    startNode = self.nodes[i]
-                    solvePath()
-                end
-            end
-
-            --[[if love.math.noise(j.x - noiseOffsetX, j.y - noiseOffsetY) > 0.92 then
-                love.graphics.setColor(0, 1, 0, 0.3)
-                love.graphics.rectangle("fill", j.x + 0.1, j.y + 0.1, 0.8, 0.8)
-                love.graphics.setColor(1, 1, 1)
-            end]]
-
-            if not j.previsited then
+            if j.visited then
                 love.graphics.setColor(1, 1, 1, 0.15)
                 love.graphics.rectangle("fill", j.x, j.y, 1, 1)
+
+                if j.isNode then
+                    love.graphics.setColor(1, 1, 1, 0.5)
+                    love.graphics.circle("fill", j.x + 0.5, j.y + 0.5, 0.15, 20)
+                end
+
                 love.graphics.setColor(1, 1, 1)
             end
 
+            if j.isNode then
+                for k, l in ipairs(j.node.neighbours) do
+                    love.graphics.line(j.x + 0.5 - _lW/2, j.y + 0.5 - _lW/2, l.x + 0.5 - _lW/2, l.y + 0.5 - _lW/2)
+                end
+            end
+
+
+            --WALLS
             if j.path[2] == 0 and not j.previsited then love.graphics.line(j.x + 1 - _lW / 2, j.y - _lW / 2, j.x + 1 - _lW / 2, j.y + 1 - _lW / 2) end
             if j.path[3] == 0 and not j.previsited then love.graphics.line(j.x - _lW / 2, j.y + 1 - _lW / 2, j.x + 1 - _lW / 2, j.y + 1 - _lW / 2) end
 
@@ -58,19 +48,29 @@ local maze = {
             end
         end
 
-        for i, j in ipairs(self.nodes) do
-            love.graphics.setColor(1, 1, 1, 0.5)
+        --[[for i, j in ipairs(self.nodes) do
+            local mx, my = love.mouse.getPosition()
+
+            if mx >= j.x * _scale + _scale and mx < j.x * _scale + _scale + _scale and my >= j.y * _scale + _scale and my < j.y * _scale + _scale + _scale then
+                if endNode ~= j and love.mouse.isDown(2) then
+                    endNode = j
+                end
+
+                if startNode ~= j and love.mouse.isDown(1) then
+                    startNode = j
+                    solvePath()
+                end
+            end
+
+            --[[love.graphics.setColor(1, 1, 1, 0.5)
+            love.graphics.circle("fill", j.x + 0.5, j.y + 0.5, 0.15, 20)
+            for k, l in ipairs(self.rooms) do
+                if j.x == l.x and j.y == l.y then
+                end
+            end
 
             if not self.rooms[i].previsited then
-                --love.graphics.circle("line", j.x + 0.5, j.y + 0.5, 0.10)
             end
-
-            love.graphics.setLineWidth(_lW * 3)
-            love.graphics.setColor(1, 0.7, 0.5, 0.1)
-            for k, l in ipairs(j.neighbours) do
-                --love.graphics.line(j.x + 0.5 - _lW * 0.66, j.y + 0.5 - _lW * 0.66, l.x + 0.5 - _lW * 0.66, l.y + 0.5 - _lW * 0.66)
-            end
-            love.graphics.setLineWidth(_lW)
 
             if j == startNode then
                 love.graphics.setColor(0, 1, 0)
@@ -81,7 +81,7 @@ local maze = {
                 love.graphics.setColor(1, 0, 0)
                 love.graphics.circle("fill", j.x + 0.5, j.y + 0.5, 0.15, 20)
             end
-        end
+        end]]
 
         love.graphics.setColor(1, 0, 0)
         local tempNode = endNode
@@ -90,8 +90,21 @@ local maze = {
             love.graphics.line(tempNode.x + 0.5 + _lW / 2, tempNode.y + 0.5 + _lW / 2, tempNode.parent.x + 0.5 + _lW / 2, tempNode.parent.y + 0.5 + _lW / 2)
             tempNode = tempNode.parent
         end
+
+        local tempNode = otherEndNode
+
+        while tempNode.parent do
+            love.graphics.line(tempNode.x + 0.5 + _lW / 2, tempNode.y + 0.5 + _lW / 2, tempNode.parent.x + 0.5 + _lW / 2, tempNode.parent.y + 0.5 + _lW / 2)
+            --love.graphics.circle("line", tempNode.x + 0.5, tempNode.y + 0.5, 0.2)
+            tempNode = tempNode.parent
+        end
     end
 }
+
+function getIndex(_modX, _modY, _element)
+    if not _element then _element = stack[#stack] end
+    return (_element.y + _modY) * width + (_element.x + _modX) + 1
+end
 
 local function initMaze(_w, _h)
     width, height = _w, _h
@@ -102,7 +115,19 @@ local function initMaze(_w, _h)
 
     for y = 0, _h - 1 do
         for x = 0, _w - 1 do
-            table.insert(maze.rooms, {x = x, y = y, visited = false, previsited = love.math.noise(x / 10 + noiseOffsetX, y / 10 + noiseOffsetY) > 0.85, path = {0, 0, 0, 0}})
+            table.insert(maze.rooms, {
+                x = x,
+                y = y,
+
+                visited = false,
+                previsited = love.math.noise(x / 10 + noiseOffsetX, y / 10 + noiseOffsetY) > 0.85,
+
+                path = {0, 0, 0, 0},
+
+                isNode = true,
+                node = nil
+            })
+
             table.insert(maze.nodes, {x = x, y = y, visited = false, neighbours = {}, parent = nil, globalScore = nil, localScore = nil})
         end
     end
@@ -110,7 +135,6 @@ local function initMaze(_w, _h)
     table.insert(stack, maze.rooms[1])
     maze.rooms[1].visited = true
 end
-
 
 local function getNeighbours(_element)
     local neighbours = {}
@@ -129,16 +153,62 @@ end
 
 local function manageNodes()
     for i, j in ipairs(maze.rooms) do
-        if not j.previsited then
+        if  (j.path[1] == 1 and j.path[3] == 1 and j.path[2] == 0 and j.path[4] == 0) or
+            (j.path[1] == 0 and j.path[3] == 0 and j.path[2] == 1 and j.path[4] == 1) then
+            j.isNode = false
+        end
+
+        if j.isNode then
+            j.node = {x = j.x, y = j.y, visited = false, neighbours = {}, parent = nil, globalScore = nil, localScore = nil}
+        end
+
+        --KEPT FOR SAFETY
+        --[[if not j.previsited and maze.nodes[i] then
             if j.path[1] == 1 then table.insert(maze.nodes[i].neighbours, maze.nodes[getIndex(0, -1, j)]) end
             if j.path[2] == 1 then table.insert(maze.nodes[i].neighbours, maze.nodes[getIndex(1, 0, j)]) end
             if j.path[3] == 1 then table.insert(maze.nodes[i].neighbours, maze.nodes[getIndex(0, 1, j)]) end
             if j.path[4] == 1 then table.insert(maze.nodes[i].neighbours, maze.nodes[getIndex(-1, 0, j)]) end
+        end]]
+    end
+
+    for i, j in ipairs(maze.rooms) do
+        if j.isNode then
+            if j.path[1] == 1 then table.insert(j.node.neighbours, findNeighbour(0, -1, j)) end
+            if j.path[2] == 1 then table.insert(j.node.neighbours, findNeighbour(1, 0, j)) end
+            if j.path[3] == 1 then table.insert(j.node.neighbours, findNeighbour(0, 1, j)) end
+            if j.path[4] == 1 then table.insert(j.node.neighbours, findNeighbour(-1, 0, j)) end
         end
     end
 
-    startNode = maze.nodes[1]
-    endNode = maze.nodes[#maze.nodes]
+    local startIndex, endIndex = 1, #maze.rooms
+    startNode = maze.nodes[startIndex]
+    endNode = maze.nodes[endIndex]
+
+    otherStartNode = maze.rooms[startIndex].node
+    otherEndNode = maze.rooms[endIndex].node
+
+    while maze.rooms[startIndex].previsited do
+        startIndex = startIndex + 1
+        startNode = maze.nodes[startIndex]
+        otherStartNode = maze.rooms[startIndex].node
+    end
+
+    while not maze.rooms[endIndex].visited do
+        endIndex = endIndex - 1
+        endNode = maze.nodes[endIndex]
+        otherEndNode = maze.rooms[endIndex].node
+    end
+end
+
+function findNeighbour(_xDir, _yDir, _element)
+    local xMod, yMod = _xDir, _yDir
+
+    while not maze.rooms[getIndex(xMod, yMod, _element)].isNode do
+        xMod = xMod + _xDir
+        yMod = yMod + _yDir
+    end
+
+    return maze.rooms[getIndex(xMod, yMod, _element)].node
 end
 
 function solvePath()
@@ -171,7 +241,58 @@ function solvePath()
             table.remove(notTestedList)
         end
 
-        if #notTestedList == 0 then return end
+        if #notTestedList == 0 then break end
+
+        currentNode = notTestedList[#notTestedList]
+        currentNode.visited = true
+
+        for i, j in ipairs(currentNode.neighbours) do
+            if not j.visited then table.insert(notTestedList, j) end
+
+            lowerGoal = currentNode.localScore + distance(currentNode, j)
+
+            if lowerGoal < j.localScore then
+                j.parent = currentNode
+                j.localScore = lowerGoal
+                j.globalScore = j.localScore + heuristic(j, endNode)
+            end
+        end
+    end
+end
+
+function solvePathAlternative()
+    for _, j in ipairs(maze.rooms) do
+        if j.isNode then
+            j.node.visited = false
+            j.node.globalScore = math.huge
+            j.node.localScore = math.huge
+            j.node.parent = nil
+        end
+    end
+
+    local function distance(a, b)
+        return math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
+    end
+
+    local function heuristic(a, b)
+        return distance(a, b)
+    end
+
+    local currentNode = otherStartNode
+    currentNode.localScore = 0
+    currentNode.globalScore = heuristic(otherStartNode, otherEndNode)
+
+    notTestedList = {}
+    table.insert(notTestedList, otherStartNode)
+
+    while #notTestedList > 0 do
+        table.sort(notTestedList, function (a, b) return a.globalScore > b.globalScore end)
+
+        while #notTestedList > 0 and notTestedList[#notTestedList].visited do
+            table.remove(notTestedList)
+        end
+
+        if #notTestedList == 0 then break end
 
         currentNode = notTestedList[#notTestedList]
         currentNode.visited = true
@@ -192,7 +313,9 @@ end
 
 function createMaze(_w, _h)
     initMaze(_w, _h)
+    local visitedCells = 0
 
+    --Carving maze
     while #stack > 0 do
         local neighbours = getNeighbours(stack[#stack])
 
@@ -227,6 +350,7 @@ function createMaze(_w, _h)
         end
     end
 
+    --Removing walls with simplex noise
     for i, j in ipairs(maze.rooms) do
         if love.math.noise(j.x - noiseOffsetX, j.y - noiseOffsetY) > 0.92 and not j.previsited then
             if j.x >= 1 then
@@ -257,10 +381,16 @@ function createMaze(_w, _h)
                 end
             end
         end
+
+        if j.visited then visitedCells = visitedCells + 1 end
     end
+
+    --Check if generation failed
+    if visitedCells < _w * _h / 2 then createMaze(_w, _h) end
 
     manageNodes()
     solvePath()
+    solvePathAlternative()
 
     return maze
 end
