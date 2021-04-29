6fe86533417484bc839117ff142cd5068512c5c5
Madarász Tamás <m.tamas1012@gmail.com>
2021-04-29 23:39:03 +0200

    removed unneeded code, moved vision here

--- enemies/enemy.lua
+++ enemies/enemy.lua
@@ -42,30 +42,11 @@ function enemy:new(_x, _y)
 			self.animations[self.state]:draw(smallExplosionImage, self.x, self.y, 0, 1, 1, 8, 8)
 		else
 			self.animations[self.state]:draw(animationImage, self.x, self.y, 0, self.direction, 1, 9, 8)
-
-			--for i, j in ipairs(self.path) do
-				--love.graphics.line(j[1], j[2], j[3], j[4])
-			--end
-
-			--[[for i = 1, #self.path - 1 do
-				love.graphics.line(self.path[i].renderX, self.path[i].renderY, self.path[i + 1].renderX, self.path[i + 1].renderY)
-			end]]
 		end
 
 		love.graphics.setColor(0, 1, 0)
 		love.graphics.rectangle("fill", self.x - 8, self.y - 10, math.max(16 * self.health / self.maxHealth, 0), 3)
 		love.graphics.setColor(1,1,1,1)
-
-		--if self.path then
-		for i = 0, 36 do
-			--love.graphics.line(self.x, self.y, self.x + self.direction * 100, self.y)
-			--love.graphics.line(self.x + self.direction * 10, self.y, self.x + self.direction * 10 + math.cos(math.pi/36 * i - math.pi/2) * 50 * self.direction, self.y + math.sin(math.pi/36 * i - math.pi/2) * 50)
-			--love.graphics.line(self.x, self.y, self.x + math.cos(math.pi/36 * i - math.pi/2) * 50 * self.direction, self.y + math.sin(math.pi/36 * i - math.pi/2) * 50)
-		end
-		love.graphics.setColor(1, 0, 0)
-		--love.graphics.print(#self.rayHitList, self.x - 8, self.y - 14)
-		love.graphics.setColor(1, 1, 1)
-		--end
 	end
 
 	function self:findPath(destination)
@@ -154,6 +135,8 @@ function enemy:new(_x, _y)
 
 			local angle = lume.angle(self.x, self.y, player.x, player.y)
 			self.footCollider:applyLinearImpulse(math.cos(angle) * -15, math.sin(angle) * -15)
+
+			player.dealtDamage = player.dealtDamage + damage
 		end
 	end
 
@@ -172,6 +155,20 @@ function enemy:new(_x, _y)
 		if #self.path <= 2 then
 			self.isAttacking = true
 			popupHandler:addElement("?", self.x, self.y - 18, {1, 0.5, 0.5})
+		end
+	end
+
+	local function worldRayCastCallback(fixture, x, y, xn, yn, fraction)
+		if fixture == player.footCollider.fixture and not self.isAttacking then
+			self:noticePlayer()
+		end
+
+		return 0
+	end
+
+	function self:lookAround()
+		for i = 0, 18 do
+			World:rayCast(self.x, self.y, self.x + math.cos(math.pi/18 * i - math.pi/2) * 100 * self.direction, self.y + math.sin(math.pi/18 * i - math.pi/2) * 100, worldRayCastCallback)
 		end
 	end
 
