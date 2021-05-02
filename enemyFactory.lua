25cae4429fdb7e1a6a9195968b590223e1161eeb
Madarász Tamás <m.tamas1012@gmail.com>
2021-05-02 21:25:37 +0200

    modified type identification

--- enemyFactory.lua
+++ enemyFactory.lua
@@ -1,7 +1,11 @@
+ENEMY_TYPES = {
+    SmallEnemy = 1
+}
+
 EnemyFactory = {}
 
 function EnemyFactory.spawnEnemy(enemyType, xCoord, yCoord)
-    if enemyType == 1 then
+    if enemyType == ENEMY_TYPES.SmallEnemy then
         return SmallEnemy:new(xCoord, yCoord)
     end
 end
