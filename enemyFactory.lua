2906346d490b6b29ae87f299852cf5b6144d6bc1
Madarász Tamás <m.tamas1012@gmail.com>
2021-05-04 16:33:32 +0200

    added new enemy type

--- enemyFactory.lua
+++ enemyFactory.lua
@@ -1,5 +1,6 @@
 ENEMY_TYPES = {
-    SmallEnemy = 1
+    SmallEnemy = {ID = 1, weight = 10},
+    SludgeEnemy = {ID = 2, weight = 20}
 }
 
 EnemyFactory = {}
@@ -7,5 +8,7 @@ EnemyFactory = {}
 function EnemyFactory.spawnEnemy(enemyType, xCoord, yCoord)
     if enemyType == ENEMY_TYPES.SmallEnemy then
         return SmallEnemy:new(xCoord, yCoord)
+    elseif enemyType == ENEMY_TYPES.SludgeEnemy then
+        return SludgeEnemy:new(xCoord, yCoord)
     end
 end
